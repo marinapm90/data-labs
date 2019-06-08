2c54e1ca93ab9af2948486d85c1ea4d00b04f1ed
marinapm90 <mpm.marinaperez@gmail.com>
2019-06-08 11:50:21 +0200

    me falta el ejercicio 2

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -1,11 +1,7 @@
 -- Challenge 1 advance SQL
 
--- Step 1 Calculate the royalties of each sales for each author
-
 SELECT titles.title_id AS 'Title ID',
 authors.au_id AS 'Author ID',
--- authors.au_lname AS 'LAST NAME',
--- authors.au_fname AS 'FIRST NAME',
 titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS 'Sales_Royalty'
 
 FROM publications.authors
@@ -21,13 +17,18 @@ ON titles.title_id = sales.title_id
 
 ORDER BY Sales_Royalty;
 
--- Step 2 Aggregate the total royalties for each title for each author
+-- Step 2 Calculate the royalties of each sales for each author.
 
-SELECT titles.title_id AS 'Title ID',
-authors.au_id AS 'Author ID',
-authors.au_lname AS 'LAST NAME',
-authors.au_fname AS 'FIRST NAME',
-sum(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS 'Aggregated_Royalties'
+-- CREO UNA TABLA TEMPORAL DEL EJERCICIO 1.
+
+drop temporary table if exists royalties_resumen;
+
+CREATE TEMPORARY TABLE royalties_resumen
+SELECT titles.title_id AS 'Title_ID',
+authors.au_id AS 'Author_ID',
+authors.au_lname AS 'LAST_NAME',
+authors.au_fname AS 'FIRST_NAME',
+titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS 'Sales_Royalty'
 
 FROM publications.authors
 
@@ -40,29 +41,77 @@ ON titleauthor.title_id = titles.title_id
 INNER JOIN sales 
 ON titles.title_id = sales.title_id
 
-GROUP BY authors.au_id, titles.title_id
-ORDER BY Aggregated_Royalties;
+ORDER BY authors.au_id, titles.title_id;
 
--- Step 3: Calculate the total profits of each author
+-- HAGO EL EJERCICIO CON LA TABLA TEMPORAL
 
 SELECT 
-authors.au_id AS 'Author ID',
-sum(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 + titles.advance) AS 'PROFITS'
+	royalties_resumen.Title_ID AS 'Title_ID',
+	royalties_resumen.Author_ID AS 'Author_ID',
+	sum(Sales_Royalty) AS 'Aggregated_Royalties'
 
-FROM publications.authors
+FROM royalties_resumen
+group by royalties_resumen.Author_ID, royalties_resumen.Title_ID;
 
-INNER JOIN titleauthor 
-ON authors.au_id = titleauthor.au_id
+-- Step 3: Calculate the total profits of each author
+-- CREO UNA TABLA TEMPORAL DEL EJERCICIO 2
+
+drop temporary table if exists tabla2;
+
+CREATE TEMPORARY TABLE tabla2
+SELECT 
+	royalties_resumen.Title_ID AS 'Title_ID',
+	royalties_resumen.Author_ID AS 'Author_ID',
+    sum(Sales_Royalty) AS 'Aggregated_Royalties'
+
+FROM royalties_resumen
+group by royalties_resumen.Author_ID, royalties_resumen.Title_ID;
+
+-- HAGO EL EJERCICIO CON LA TABLA TEMPORAL
+
+SELECT
+tabla2.Title_ID,
+tabla2.Author_ID,
+sum(Aggregated_Royalties + titles.advance) AS 'PROFITS'
+
+FROM tabla2
 
 INNER JOIN titles 
-ON titleauthor.title_id = titles.title_id
+ON tabla2.Title_ID = titles.title_id
 
-INNER JOIN sales 
-ON titles.title_id = sales.title_id
-
-GROUP BY authors.au_id, titles.title_id
+GROUP BY tabla2.Author_ID, tabla2.Title_ID
 ORDER BY PROFITS DESC
 LIMIT 3;
 
--- Challenge 2 - Alternative Solution
+-- Challenge 2
+
+-- ME FALTA HACER ESTE EJERCICIO
+
+-- Challenge 3
+
+-- CREO UNA TABLA PERMANENTE DEL PASO 3
+
+CREATE TABLE most_profiting_authors
+
+SELECT
+tabla2.Title_ID,
+tabla2.Author_ID,
+sum(Aggregated_Royalties + titles.advance) AS 'PROFITS'
+
+FROM tabla2
+
+INNER JOIN titles 
+ON tabla2.Title_ID = titles.title_id
+
+GROUP BY tabla2.Author_ID, tabla2.Title_ID
+ORDER BY PROFITS DESC
+LIMIT 3;
+
+-- USO LA TABLA PERMANENTE
+
+SELECT
+most_profiting_authors.Author_ID,
+most_profiting_authors.PROFITS
+
+FROM most_profiting_authors;
 
